1b27042132b1af8d58ea59981fb042d473d5acd5
Spshake <amx_cfg@hotmail.com>
2019-04-28 19:53:18 -0300

    Login de usuario
    Agregué: El logo, a los campos de Texto les asigné según correspondía condiciones, por ejemplo, el de usuario sólo se puede tipear de la A a la Z.
    Cuando se acepta los campos debene star llenos sino sale un cartel que dice que deben ser llenados.

--- src/org/pizzeria/pizzeria/ui/Login.java
+++ src/org/pizzeria/pizzeria/ui/Login.java
@@ -16,6 +16,9 @@ public class Login extends javax.swing.JFrame {
      */
     public Login() {
         initComponents();
+        
+        this.setTitle ("Login de Usuarios");
+        this.setLocation(600,200);
     }
 
     /**
@@ -29,83 +32,103 @@ public class Login extends javax.swing.JFrame {
 
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        log_usuario = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jPasswordField1 = new javax.swing.JPasswordField();
+        btn_reset = new javax.swing.JButton();
+        btn_aceptar = new javax.swing.JButton();
+        log_pass = new javax.swing.JPasswordField();
+        jLabel4 = new javax.swing.JLabel();
+        tipo_usuario = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setText("¿Quién so vo?");
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Ugis.jpg"))); // NOI18N
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 460, 190));
 
         jLabel2.setText("Usuario");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 220, -1, -1));
 
-        jLabel3.setText("Clave");
-
-        jButton1.setText("Reset");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        log_usuario.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                log_usuarioActionPerformed(evt);
             }
         });
+        log_usuario.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                log_usuarioKeyTyped(evt);
+            }
+        });
+        getContentPane().add(log_usuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 220, 137, -1));
 
-        jButton2.setText("Aceptar");
+        jLabel3.setText("Clave");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 260, -1, -1));
 
-        jPasswordField1.setText("jPasswordField1");
+        btn_reset.setText("Reset");
+        btn_reset.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_resetActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btn_reset, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 260, -1, -1));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel2)
-                                    .addComponent(jLabel3))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(jPasswordField1)
-                                    .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 137, Short.MAX_VALUE))))
-                        .addGap(116, 116, 116))
-                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                        .addGap(34, 34, 34)
-                        .addComponent(jButton1)
-                        .addGap(73, 73, 73)
-                        .addComponent(jButton2)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(25, 25, 25)
-                .addComponent(jLabel1)
-                .addGap(36, 36, 36)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel3)
-                    .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 57, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
-                .addGap(35, 35, 35))
-        );
+        btn_aceptar.setText("Aceptar");
+        btn_aceptar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_aceptarActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btn_aceptar, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 260, -1, -1));
+
+        log_pass.setText("jPasswordField1");
+        getContentPane().add(log_pass, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 260, 137, -1));
+
+        jLabel4.setText("Acceso");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 220, -1, -1));
+
+        tipo_usuario.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Recepción", "Cocina", "Cliente", "Encargado", " " }));
+        tipo_usuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                tipo_usuarioActionPerformed(evt);
+            }
+        });
+        getContentPane().add(tipo_usuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 220, 120, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btn_resetActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_resetActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btn_resetActionPerformed
+
+    private void log_usuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_log_usuarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_log_usuarioActionPerformed
+
+    private void tipo_usuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tipo_usuarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_tipo_usuarioActionPerformed
+
+    private void log_usuarioKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_log_usuarioKeyTyped
+        char c = evt.getKeyChar();
+if ((c<'a'||c>'z') && (c<'A'||c>'Z')&&(c<' '||c>' '))evt.consume();
+    }//GEN-LAST:event_log_usuarioKeyTyped
+
+    private void btn_aceptarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_aceptarActionPerformed
+        String cadena2,cadena3,cadena4;
+        
+        cadena2 = log_usuario.getText();
+        cadena3 = log_pass.getText().toString();
+        cadena4 = tipo_usuario.getSelectedItem().toString();
+        
+                if (log_usuario.getText().equals("") || (log_pass.getText().equals("")) || (tipo_usuario.getSelectedItem().equals(null))) {
+            
+            javax.swing.JOptionPane.showMessageDialog(this,"Debe llenar todos los campos \n","AVISO!",javax.swing.JOptionPane.INFORMATION_MESSAGE);
+            log_usuario.requestFocus();
+        }
+
+
+    }//GEN-LAST:event_btn_aceptarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -143,12 +166,14 @@ public class Login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton btn_aceptar;
+    private javax.swing.JButton btn_reset;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JPasswordField jPasswordField1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPasswordField log_pass;
+    private javax.swing.JTextField log_usuario;
+    private javax.swing.JComboBox<String> tipo_usuario;
     // End of variables declaration//GEN-END:variables
 }
